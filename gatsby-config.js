bcd4197677f7e5e376450214397fc1b0850fa6ba
Taehong Kim <lkoiescg2031@naver.com>
2021-01-19 21:02:13 +0900

    [Add] siteMetadata 추가

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,21 @@
 module.exports = {
   siteMetadata: {
-    title: `Happy Life Programmer`,
-    description: `My life log`,
-    author: `taehong kim`,
-    email: `lkoiescg2031@naver.com`,
-    github: `github.com/lkoiescg2031`,
+    title: 'Happy Life Programmer',
+    description: 'My life log',
+    author: 'taehong kim',
+    user: {
+      name: 'taehong kim',
+      figure:
+        'https://avatars3.githubusercontent.com/u/42562726?s=400&u=611e46e8fb672c2be8df5c50ad9f2e5c3a35cbbf&v=4',
+      position: 'developer',
+      email: 'lkoiescg2031@naver.com',
+      blog: 'https://lkoiescg2031.github.io',
+      github: 'https://github.com/lkoiescg2031',
+      facebook: 'https://www.facebook.com/profile.php?id=100015452532299',
+      twitter: '',
+      instagram: '',
+      linkedIn: '',
+    },
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
