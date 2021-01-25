e4b700d4f197fdd7e822d461caecb3226e05a4e9
Taehong Kim <lkoiescg2031@naver.com>
2021-01-26 00:51:56 +0900

    [Modify] Layout 변경

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,14 +2,15 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import SEO from '../components/seo';
+import BlogLayout from '../layout/BlogLayout';
 
 const SecondPage = () => (
-  <div>
+  <BlogLayout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  </div>
+  </BlogLayout>
 );
 
 export default SecondPage;
--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import { PageProps, Link, graphql } from "gatsby"
 
 import SEO from "../components/seo"
+import BlogLayout from "../layout/BlogLayout"
 
 type DataProps = {
   site: {
@@ -11,7 +12,7 @@ type DataProps = {
 }
 
 const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
-  <div>
+  <BlogLayout>
     <SEO title="Using TypeScript" />
     <h1>Gatsby supports TypeScript by default!</h1>
     <p>This means that you can create and write <em>.ts/.tsx</em> files for your pages, components etc. Please note that the <em>gatsby-*.js</em> files (like gatsby-node.js) currently don't support TypeScript yet.</p>
@@ -19,7 +20,7 @@ const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
     <p>You're currently on the page "{path}" which was built on {data.site.buildTime}.</p>
     <p>To learn more, head over to our <a href="https://www.gatsbyjs.com/docs/typescript/">documentation about TypeScript</a>.</p>
     <Link to="/">Go back to the homepage</Link>
-  </div>
+  </BlogLayout>
 )
 
 export default UsingTypescript
