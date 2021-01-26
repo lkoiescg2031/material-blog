d45ea02117bc0c206b67d38ba9911f6f7a1fc659
Taehong Kim <lkoiescg2031@naver.com>
2021-01-26 19:17:51 +0900

    [Modify] WaveAniBackground 컴포넌트로 변경

--- src/components/WaveAniBackground/index.jsx
+++ src/components/WaveAniBackground/index.jsx
@@ -1,100 +1,107 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-
-import { StyleSheet, css } from 'aphrodite';
 import WaveGroup from './WaveGroup';
+import { StyleSheet, css } from 'aphrodite';
 
-const backgroundStyle = StyleSheet.create({
+const styles = StyleSheet.create({
   canvas: {
     position: 'absolute',
     top: 0,
     left: 0,
-    zIndex: -99999,
+    zIndex: -9999,
   },
 });
 
-export default function WaveBackgroundAni(props) {
-  const {
-    waveCount,
-    pointCount,
-    waveHeight,
-    waveMaxHeight,
-    speed,
-    colors,
-    ...restProps
-  } = props;
-
-  const canvasRef = React.useRef();
-  const waveGroup = new WaveGroup(
-    waveCount,
-    pointCount,
-    waveHeight,
-    waveMaxHeight,
-    speed,
-    colors,
-  );
-
-  //resize
-  const ___resize = () => {
-    const width = document.body.clientWidth;
-    const height = document.body.clientHeight;
-
-    //for ratina
-    const canvas = canvasRef.current;
-    if (typeof canvas !== 'undefined' && canvas !== null) {
-      const context = canvas.getContext('2d');
-      const ratio = 1;
-      // const { devicePixelRatio: ratio = 1 } = window;
-
-      canvas.width = width * ratio;
-      canvas.height = height * ratio;
-      context.scale(ratio, ratio);
-    }
-
-    waveGroup.resize(width, height);
+export default class WaveAniBackground extends PureComponent {
+  static propTypes = {
+    props: PropTypes,
+    waveCount: PropTypes.number,
+    pointCount: PropTypes.number,
+    waveHeight: PropTypes.number,
+    waveMaxHeight: PropTypes.number,
+    speed: PropTypes.number,
+    colors: PropTypes.arrayOf(PropTypes.string),
   };
 
-  window.addEventListener('resize', ___resize.bind(this));
+  static defaultProps = {};
 
-  //animation
-  const ___draw = ctx => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    waveGroup.draw(ctx);
-  };
+  constructor(props) {
+    super(props);
 
-  React.useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
+    this.state = {};
 
-    ___resize();
+    this.canvasRef = React.createRef();
 
-    let animationFrameId = 0;
+    this.init = this.initializeCanvas.bind(this);
+    this.clear = this.clear.bind(this);
+    this.run = this.run.bind(this);
+    this.resize = this.resize.bind(this);
+    this.animate = this.animate.bind(this);
+  }
 
-    const animate = t => {
-      ___draw(ctx);
-      animationFrameId = window.requestAnimationFrame(animate);
-    };
+  clear() {
+    const {
+      waveCount,
+      pointCount,
+      waveHeight,
+      waveMaxHeight,
+      speed,
+      colors,
+    } = this.props;
 
-    animate();
+    this.waveGroupAni = new WaveGroup(
+      waveCount,
+      pointCount,
+      waveHeight,
+      waveMaxHeight,
+      speed,
+      colors,
+    );
+  }
+  run() {
+    window.addEventListener('resize', this.resize, false);
+    this.resize();
 
-    return () => {
-      window.cancelAnimationFrame(animationFrameId);
-    };
-  }, [___draw, ___resize]);
+    this.requestAnimationFrameId = window.requestAnimationFrame(this.animate);
+  }
 
-  return (
-    <canvas
-      ref={canvasRef}
-      className={css(backgroundStyle.canvas)}
-      {...restProps}
-    />
-  );
+  resize() {
+    this.stageWidth = document.body.clientWidth;
+    this.stageHeight = document.body.clientHeight;
+
+    //for retina
+    const ratio = 1; //window.devicePixelRatio || 1;
+    this.canvas.width = this.stageWidth * ratio;
+    this.canvas.height = this.stageHeight * ratio;
+    this.ctx.scale(ratio, ratio);
+
+    this.waveGroupAni.resize(this.stageWidth, this.stageHeight);
+  }
+
+  animate() {
+    //prevAnimation
+    this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
+
+    this.waveGroupAni.draw(this.ctx);
+
+    //afterAnimation
+    this.requestAnimationFrameId = window.requestAnimationFrame(this.animate);
+  }
+
+  componentDidMount() {
+    this.initializeCanvas();
+    this.run();
+  }
+
+  initializeCanvas() {
+    this.canvas = this.canvasRef.current;
+    this.ctx = this.canvas.getContext('2d');
+
+    this.requestAnimationFrameId = 0;
+    this.clear();
+  }
+
+  render() {
+    return <canvas ref={this.canvasRef} className={css(styles.canvas)} />;
+  }
 }
-
-WaveBackgroundAni.propTypes = {
-  waveCount: PropTypes.number,
-  pointCount: PropTypes.number,
-  waveHeight: PropTypes.func,
-  waveMaxHeight: PropTypes.func,
-  color: PropTypes.arrayOf(PropTypes.string),
-};
